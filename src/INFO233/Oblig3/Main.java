4ed40aad58333d854aec46eca1da4b28c7d9839b
matssh <mats.hagen2@gmail.com>
2018-04-05 14:52:27 +0200

    Lagt til database-filen

--- src/INFO233/Oblig3/Main.java
+++ src/INFO233/Oblig3/Main.java
@@ -1,8 +1,36 @@
 package INFO233.Oblig3;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Main {
+
+    public static void createNewDatabase(String filename){
+        try{
+            Class.forName("org.sqlite.JDBC");
+        }
+        catch (ClassNotFoundException e){
+            System.out.println("Feil i lasting av driver " + e);
+        }
+
+        String url = "jdbc:sqlite:" + filename;
+        try {
+            Connection con = DriverManager.getConnection(url);
+            if (con != null){
+                DatabaseMetaData meta = con.getMetaData();
+                System.out.println("Navn på driver: " + meta.getDriverName());
+                System.out.println("Database opprettet");
+            }
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
 
     public static void main(String[] args) {
 	// write your code here
+        createNewDatabase("oblig3v1_database.sql");
     }
 }
